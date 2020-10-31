cd0b682f07651ffa06fc831cd2b38bcaf3b30703
Gabriel <gabrielescobar15@gmail.com>
2020-10-31 18:29:35 -0400

    nb combinando padrões em 2 sentenças

--- nb_utils.py
+++ nb_utils.py
@@ -1,10 +1,27 @@
 import pandas as pd
 import numpy as np
 import torch
+method_names = {'word2vec': 'Word2vec C', 'summation_dot_product': 'DIVE \u0394S * C ', 'dot_product': 'DIVE C',
+                'rnd': 'random', 'summation': 'DIVE \u0394S', 'summation_word2vec': 'DIVE \u0394S * Word2vec C',
+                'all_subword mean_positional_rank': 'BERT Mean Pos Rank', 'all_subword min_positional_rank': 'BERT '
+                                                                                                             'Min Pos '
+                                                                                                             'Rank',
+                'all_subword max_pattern': 'BERT Max Pattern', 'all_subword mean_pattern': 'BERT Mean Pattern',
+                'min score_final_log(z)': 'BERT Min Pos Rank (log(z))', 'min score_final_norm': 'BERT Min Pos Rank (/ '
+                                                                                                'norm)',
+                'mean score_final_log(z)': 'BERT Mean Pos Rank (log(z))', 'mean score_final_norm': 'BERT Mean Pos '
+                                                                                                   'Rank (/ norm)',
+                'min bert_soma_total': 'BERT Min Sum', 'mean bert_soma_total': 'BERT Mean Sum'}
 
 
-def create_dataframe(json_dict):
-    dict_values = {'hiponimo': [], 'hiperonimo':[], 'classe':[], 'fonte':[], 'pattern': [], 'soma_hipo':[], 'soma_hiper':[], 'len_hipo':[], 'len_hiper':[]}
+# if combination
+# [[hipo 1st sen], [hipo 2nd sen], [hyper 1st sen], [hyper 2nd sen]]
+# else
+# [[hipo 1st sen], [hyper 1st sen]]
+
+def create_dataframe(json_dict, combination=False):
+    dict_values = {'hiponimo': [], 'hiperonimo': [], 'classe': [], 'fonte': [], 'pattern': [], 'soma_hipo': [],
+                   'soma_hiper': [], 'len_hipo': [], 'len_hiper': []}
     for data, values in json_dict.items():
         hipo, hiper, classe, fonte = data.strip().split()
         for pattern, score in values.items():
@@ -13,20 +30,30 @@ def create_dataframe(json_dict):
             dict_values['classe'].append(classe)
             dict_values['fonte'].append(fonte)
             dict_values['pattern'].append(pattern)
-            soma = sum(score[0])
+            if combination:
+                soma = sum(score[0]) + sum(score[1])
+            else:
+                soma = sum(score[0])
             dict_values['soma_hipo'].append(soma)
-            soma = sum(score[1])
+            if combination:
+                soma = sum(score[2]) + sum(score[3])
+            else:
+                soma = sum(score[1])
             dict_values['soma_hiper'].append(soma)
-            dict_values['len_hipo'].append(len(score[0]))
-            dict_values['len_hiper'].append(len(score[1]))
+            if combination:
+                dict_values['len_hipo'].append(len(score[0]))
+                dict_values['len_hiper'].append(len(score[2]))
+            else:
+                dict_values['len_hipo'].append(len(score[0]))
+                dict_values['len_hiper'].append(len(score[1]))
 
     df = pd.DataFrame(dict_values)
     df['bert_soma_total'] = df['soma_hipo'] + df['soma_hiper']
     df['len_total'] = df['len_hipo'] + df['len_hiper']
     return df
 
+
 def filter_by_vocab(path_vocab, dict_data):
-    wiki_name = path_vocab.split("/")[-2][9:]
     new_data = {}
     vocab = []
     for line in open(path_vocab):
@@ -40,12 +67,13 @@ def filter_by_vocab(path_vocab, dict_data):
             new_data[k] = v.copy()
     return new_data
 
+
 # devolve df de balanceamento True/False por tamanho de subtoken
 def balanceamento(df, len_size, patterns):
     df_rate = df[df['pattern'] == patterns[0]][['hiponimo', 'hiperonimo', 'classe', 'fonte', 'len_total']]
     df_rate = df_rate.groupby(['len_total'])['classe'].value_counts()
 
-    dict_values = {'len_total': [], 'true':[], 'false':[]}
+    dict_values = {'len_total': [], 'true': [], 'false': []}
     for v in len_size:
         if v in df_rate:
             dict_values['len_total'].append(v)
@@ -66,7 +94,7 @@ def balanceamento(df, len_size, patterns):
     return df_taxa
 
 
-#logsumexp para cada tamanho subtoken e normalização
+# logsumexp para cada tamanho subtoken e normalização
 def logsumexp_normalization(df_data, len_list, pattern_list):
     df = df_data.copy()
     log_store = {}
@@ -131,7 +159,7 @@ def compute_AP_by_rank(df, key_sort, best_patterns):
         df_sorted = df[df['pattern'] == p]
         df_sorted = df_sorted.sort_values(by=key_sort, ascending=False, ignore_index=True)
         for row in df_sorted.itertuples():
-            name  = f"{row.hiponimo} {row.hiperonimo} {row.classe} {row.fonte}"
+            name = f"{row.hiponimo} {row.hiperonimo} {row.classe} {row.fonte}"
             if name in rank:
                 rank[name].append(row.Index)
             else:
@@ -160,7 +188,7 @@ def compute_AP_by_rank(df, key_sort, best_patterns):
 
 
 def compute_AP_n_best_pattern(df, key_sort, n_best_pattern):
-    dict_values = {'n_best_pattern': [], 'method': [] ,'AP':[]}
+    dict_values = {'n_best_pattern': [], 'method': [], 'AP': []}
     for num_p in range(1, len(n_best_pattern) + 1):
         min_ap, mean_ap = compute_AP_by_rank(df=df, key_sort=key_sort, best_patterns=n_best_pattern[:num_p])
         dict_values['n_best_pattern'].append(num_p)
